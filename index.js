44a7014ad3fa10a2b5c4147d255f49344c279b7d
NicolasRitouet <nicolas@ritouet.com>
2016-10-22 23:17:32 +0200

    Add comments and repo in package.json

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+// External dependencies
 const Koa = require('koa');
 const bodyparser = require('koa-bodyparser');
 const logger = require('koa-logger');
+
+// Internal modules
 const routes = require('./routes');
 const config = require('./config')();
-const app = new Koa();
 
+// Setup the app
+const app = new Koa();
 
 // Add middleware
 app.use(logger())
@@ -16,5 +20,8 @@ app.use(routes.baseRoutes.routes())
   .use(routes.secondRoutes.routes())
   .use(routes.secondRoutes.allowedMethods());
 
+// Start server
 app.listen(config.port, () => console.log(`Server started ${ config.port }`));
+
+// Export server to allow testing
 module.exports = app;
